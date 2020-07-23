b91db11248b9395890708502e9ef7214da9c9f1d
Mauricio Collazos <ma0acollazos@gmail.com>
2020-07-22 22:48:31 -0500

    I have not idea what I'm doing

--- automatic_segmentation/silence_segmentation.py
+++ automatic_segmentation/silence_segmentation.py
@@ -33,23 +33,31 @@ def write_text_grid_from_segmentation(segmentation, text_name, output_folder, xm
     tg.write(os.path.join(output_folder, f"automatic_{text_name}.TextGrid"))
 
 
+def evaluate_for_single_file(wav_folder, output_folder, wav_file):
+    frequency, signal = wavfile.read(os.path.join(wav_folder, wav_file))
+    LOGGER.debug(f"Processing {wav_file}: frequency: {frequency}")
+    segmentation = silence_segmentation.silence_segmentation(signal, frequency, 0.15)
+    write_text_grid_from_segmentation(
+        segmentation,
+        wav_file.replace(".wav", ""),
+        output_folder,
+        xmax=signal.shape[-1] / frequency,
+        audio_frequency=frequency
+    )
+
+
 def create_automatic_segmentation(wav_folder, output_folder):
 
     for wav_file in os.listdir(wav_folder):
-        frequency, signal = wavfile.read(os.path.join(wav_folder, wav_file))
-        LOGGER.debug(f"Processing {wav_file}: frequency: {frequency}")
-        segmentation = silence_segmentation.silence_segmentation(signal, frequency, 0.3)
-        write_text_grid_from_segmentation(
-            segmentation,
-            wav_file.replace(".wav", ""),
-            output_folder,
-            xmax=signal.shape[-1] / frequency,
-            audio_frequency=frequency
-        )
+        evaluate_for_single_file(wav_folder, output_folder, wav_file)
         break
+
 
 if __name__ == "__main__":
     print(sys.argv)
+    if len(sys.argv) == 4:
+        evaluate_for_single_file(sys.argv[1], sys.argv[2], sys.argv[3])
+        exit(0)
     if len(sys.argv) != 3:
         LOGGER.error("Only 2 arg accepted")
         exit(1)
